f7c883bc36285a39b48a13e57bf8dc7d3e0081eb
nguyenducthanh04 <dducthanh04@gmail.com>
2024-08-03 18:40:33 +0700

    update

--- Controller/BlogController.js
+++ Controller/BlogController.js
@@ -6,29 +6,49 @@ const Comment = model.comment;
 const MarkDownBlog = model.MarkDownBlog;
 class BlogController {
     async getAllBlog(req, res) {
-        const allBlog = await MarkDownBlog.findAll();
-        res.json(allBlog);
+        try {
+            const allBlog = await MarkDownBlog.findAll();
+            res.json(allBlog);
+        } catch (error) {
+            console.error("Error incrementing view count:", error);
+            res.status(500).json({ message: "Server error" });
+        }
     }
     async getBlogByTitle(req, res) {
-        const { title } = req.params;
-        const blogByTitle = await MarkDownBlog.findOne({
-            where: {
-                title: title,
-            },
-        });
-        res.json(blogByTitle);
+        try {
+            const { title } = req.params;
+            const blogByTitle = await MarkDownBlog.findOne({
+                where: {
+                    title: title,
+                },
+            });
+            res.json(blogByTitle);
+        } catch (error) {
+            console.error("Error incrementing view count:", error);
+            res.status(500).json({ message: "Server error" });
+        }
     }
     async createBlog(req, res) {
-        const { title, description, urlImageBanner, contentMD, contentHTML } =
-            req.body;
-        const newBlog = await MarkDownBlog.create({
-            title: title,
-            description: description,
-            urlImageBanner: urlImageBanner,
-            contentHTML: contentHTML,
-            contentMarkdown: contentMD,
-        });
-        res.json(newBlog);
+        try {
+            const {
+                title,
+                description,
+                urlImageBanner,
+                contentMD,
+                contentHTML,
+            } = req.body;
+            const newBlog = await MarkDownBlog.create({
+                title: title,
+                description: description,
+                urlImageBanner: urlImageBanner,
+                contentHTML: contentHTML,
+                contentMarkdown: contentMD,
+            });
+            res.json(newBlog);
+        } catch (error) {
+            console.error("Error incrementing view count:", error);
+            res.status(500).json({ message: "Server error" });
+        }
     }
     async incrementView(req, res) {
         const { title } = req.params;
@@ -51,70 +71,95 @@ class BlogController {
         }
     }
     async addComment(req, res) {
-        const { blogId, userId, commentText } = req.body;
-        const newComment = await Comment.create({
-            blogId: blogId,
-            userId: userId,
-            commentText: commentText,
-            parentCommentId: 0,
-        });
-        res.json(newComment);
+        try {
+            const { blogId, userId, commentText } = req.body;
+            const newComment = await Comment.create({
+                blogId: blogId,
+                userId: userId,
+                commentText: commentText,
+                parentCommentId: 0,
+            });
+            res.json(newComment);
+        } catch (error) {
+            console.error("Error incrementing view count:", error);
+            res.status(500).json({ message: "Server error" });
+        }
     }
     async repComment(req, res) {
-        const { blogId, userId, commentText, parentCommentId } = req.body;
-        const newComment = await Comment.create({
-            blogId: blogId,
-            userId: userId,
-            commentText: commentText,
-            parentCommentId: parentCommentId,
-        });
-        res.json(newComment);
+        try {
+            const { blogId, userId, commentText, parentCommentId } = req.body;
+            const newComment = await Comment.create({
+                blogId: blogId,
+                userId: userId,
+                commentText: commentText,
+                parentCommentId: parentCommentId,
+            });
+            res.json(newComment);
+        } catch (error) {
+            console.error("Error incrementing view count:", error);
+            res.status(500).json({ message: "Server error" });
+        }
     }
     async getCommentByBlog(req, res) {
-        const { blogId } = req.params;
-        const comments = await Comment.findAll({
-            where: {
-                blogId: blogId,
-                parentCommentId: 0,
-            },
-            include: [
-                {
-                    model: User,
+        try {
+            const { blogId } = req.params;
+            const comments = await Comment.findAll({
+                where: {
+                    blogId: blogId,
+                    parentCommentId: 0,
                 },
-            ],
-        });
-        res.json(comments);
+                include: [
+                    {
+                        model: User,
+                    },
+                ],
+            });
+            res.json(comments);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: "An error occurred" });
+        }
     }
     async getCommentChild(req, res) {
-        const { blogId } = req.params;
-        const commentChild = await Comment.findAll({
-            where: {
-                blogId: blogId,
-                parentCommentId: {
-                    [Op.ne]: 0,
+        try {
+            const { blogId } = req.params;
+            const commentChild = await Comment.findAll({
+                where: {
+                    blogId: blogId,
+                    parentCommentId: {
+                        [Op.ne]: 0,
+                    },
                 },
-            },
-            include: [
-                {
-                    model: User,
-                },
-            ],
-        });
-        res.json(commentChild);
+                include: [
+                    {
+                        model: User,
+                    },
+                ],
+            });
+            res.json(commentChild);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: "An error occurred" });
+        }
     }
     async editComment(req, res) {
-        const { commentId, content } = req.body;
-        const editComment = await Comment.update(
-            {
-                commentText: content,
-            },
-            {
-                where: {
-                    id: commentId,
+        try {
+            const { commentId, content } = req.body;
+            const editComment = await Comment.update(
+                {
+                    commentText: content,
                 },
-            }
-        );
-        res.json(editComment);
+                {
+                    where: {
+                        id: commentId,
+                    },
+                }
+            );
+            res.json(editComment);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: "An error occurred" });
+        }
     }
     async deleteComment(req, res) {
         try {
